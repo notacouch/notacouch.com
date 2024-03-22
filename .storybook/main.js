c089f2efffa071b6df7939210e4b051effc96ed7
Abed Islam <versatil@gmail.com>
2024-03-22 15:55:04 -0400

    sb update: axe-core a11y add-on implemented =)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,19 @@
 /** @type { import('@storybook/html-vite').StorybookConfig } */
 const config = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    '@storybook/addon-a11y',
+    '@storybook/addon-interactions',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
   ],
   framework: {
-    name: "@storybook/html-vite",
+    name: '@storybook/html-vite',
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
   },
   async viteFinal(config, { configType }) {
     const { mergeConfig } = await import('vite');
@@ -30,14 +28,14 @@ const config = {
       // Your environment configuration here
       server: {
         watch: {
-          ignored: ['_site/**']
-        }
+          ignored: ['_site/**'],
+        },
       },
       build: {
         rollupOptions: {
-          external:['_site/**']
-        }
-      }
+          external: ['_site/**'],
+        },
+      },
     });
   },
 };
